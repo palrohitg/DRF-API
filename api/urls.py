6edbd12516f37a70471ad174d48c986d9fa15a1a
Vikas Pal <rohitprotobit@gmail.com>
2021-06-16 14:14:34 +0530

    urls swagger io

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-urlpatterns = [
-    path('', views.apiOverview, name="api-overview"), 
-    path('blog-list/', views.blogList, name="blog-list"), 
-    path('blog-details/<str:pk>', views.blogDetails, name="blog-details"), 
-    path('blog-update/<str:pk>', views.blogUpdate, name="blog-update"), 
-    path('blog-create/', views.blogCreate, name="blog-create"), 
-    path('blog-delete/<str:pk>', views.blogDelete, name="blog-delete"), 
-]
 
+
+urlpatterns = [
+    path('', views.apiOverview, name="api-overview"),
+    path('blog-list/', views.blogList, name="blog-list"),
+    path('blog-details/<str:pk>', views.blogDetails, name="blog-details"),
+    path('blog-update/<str:pk>', views.blogUpdate, name="blog-update"),
+    path('blog-create/', views.blogCreate, name="blog-create"),
+    path('blog-delete/<str:pk>', views.blogDelete, name="blog-delete"),
+    # path('^swagger(?P<format>\.json|\.yaml)$',
+    #      schema_view.without_ui(cache_timeout=0), name='schema-json'),
+]
